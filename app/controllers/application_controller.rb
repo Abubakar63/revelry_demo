d89fc980899d8f4c53b2bdb19b48476813e0f962
Zeeshan Gulzar <zeeshan.gulzar@square63.com>
2016-09-20 11:23:06 +0500

    Added authentication and default format json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate_token
+  before_filter :set_default_response_format
+
+  private
+
+    def authenticate_token
+      unless params['token'].present? && params['token'] == ENV['AUTH_TOKEN']
+        head :unauthorized
+      end
+    end
+
+    def set_default_response_format
+      request.format = :json
+    end
+
 end
